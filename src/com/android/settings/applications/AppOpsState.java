rohithbijoy21/packages_apps_Settings

AppOps: use custom arrays and strings without summaries
Change-Id: I63d16aa9b56907b18ed1196f8d76274ebb6bc077

src/com/android/settings/applications/AppOpsState.java
@@ -56,8 +56,8 @@ public class AppOpsState {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
-        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_labels_custom);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_custom);
     }
 
     public static class OpsTemplate implements Parcelable {

src/com/android/settings/applications/AppOpsSummary.java
@@ -107,7 +107,7 @@ public class AppOpsSummary extends InstrumentedPreferenceFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_custom);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());

manishjvora/CANDY_packages_apps_Settings

src/com/android/settings/applications/AppOpsState.java
@@ -56,8 +56,8 @@ public class AppOpsState {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
-        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_labels_aosip);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_aosip);
     }
 
     public static class OpsTemplate implements Parcelable {

src/com/android/settings/applications/AppOpsSummary.java
@@ -104,7 +104,7 @@ public class AppOpsSummary extends InstrumentedPreferenceFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_aosip);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());