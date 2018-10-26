StatiXOS/android_packages_apps_Settings

Ensure everything plays nice
- Make system accent/theme selectors unavailable as well when substratum is detected
- Make use of the methods in our Utils/fwb

Change-Id: Ied7ddbeb33e427461c78bf14565c7bfb30468ee0

src/com/android/settings/display/AccentPickerPreferenceController.java
@@ -32,10 +32,14 @@ import com.android.settingslib.core.lifecycle.events.OnResume;
 
 import com.dirtyunicorns.tweaks.fragments.AccentPicker;
 
+import com.android.internal.util.du.Utils;
+
 public class AccentPickerPreferenceController extends AbstractPreferenceController
         implements PreferenceControllerMixin, LifecycleObserver, OnResume {
 
     private static final String KEY_ACCENT_PICKER_FRAGMENT_PREF = "accent_picker";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private static final int MY_USER_ID = UserHandle.myUserId();
 
     private final Fragment mParent;
@@ -52,6 +56,11 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mAccentPickerPref  = (Preference) screen.findPreference(KEY_ACCENT_PICKER_FRAGMENT_PREF);
+        if (!Utils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+            mAccentPickerPref.setEnabled(true);
+        } else {
+            mAccentPickerPref.setEnabled(false);
+        }
     }
 
     @Override
@@ -79,16 +88,25 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
             new OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    AccentPicker.show(mParent);
-                    return true;
+                   if (!Utils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+                        AccentPicker.show(mParent);
+                        return true;
+                   } else {
+                        return false;
+                   }
                 }
             });
     }
 
     public void updateSummary() {
         if (mAccentPickerPref != null) {
+            if (!Utils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
                 mAccentPickerPref.setSummary(mContext.getString(
                         com.android.settings.R.string.theme_accent_picker_summary));
+            } else {
+                mAccentPickerPref.setSummary(mContext.getString(
+                        com.android.settings.R.string.disable_accents_installed_title));
+            }
         }
     }
 }

src/com/android/settings/display/SystemThemePreferenceController.java
@@ -18,26 +18,30 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.TextUtils;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.provider.Settings;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.android.settings.R;
-
 import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
+
+import com.android.internal.util.du.Utils;
 
 import libcore.util.Objects;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SystemThemePreferenceController extends AbstractPreferenceController implements
-        Preference.OnPreferenceChangeListener {
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
 
     private static final String SYSTEM_THEME = "system_theme_style";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private ListPreference mSystemThemeStyle;
 
     public SystemThemePreferenceController(Context context) {
@@ -58,14 +62,18 @@ public class SystemThemePreferenceController extends AbstractPreferenceControlle
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mSystemThemeStyle = (ListPreference) screen.findPreference(SYSTEM_THEME);
-        int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_THEME, 0);
-        int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
-        mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
-        mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        if (!Utils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+            int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_THEME, 0);
+            int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
+            mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
+            mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        } else {
+            mSystemThemeStyle.setEnabled(false);
+            mSystemThemeStyle.setSummary(R.string.disable_themes_installed_title);
+        }
     }
-
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mSystemThemeStyle) {
@@ -80,7 +88,6 @@ public class SystemThemePreferenceController extends AbstractPreferenceControlle
         }
         return true;
     }
-
     private void reload(){
         Intent intent2 = new Intent(Intent.ACTION_MAIN);
         intent2.addCategory(Intent.CATEGORY_HOME);

manishjvora/CANDY_packages_apps_Settings

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

src/com/android/settings/display/AccentPickerPreferenceController.java
@@ -30,12 +30,16 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import com.aosip.owlsnest.AccentPicker;
+import org.candy.candyshop.fragments.AccentPicker;
+
+import com.android.internal.util.candy.CandyUtils;
 
 public class AccentPickerPreferenceController extends AbstractPreferenceController
         implements PreferenceControllerMixin, LifecycleObserver, OnResume {
 
     private static final String KEY_ACCENT_PICKER_FRAGMENT_PREF = "accent_picker";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private static final int MY_USER_ID = UserHandle.myUserId();
 
     private final Fragment mParent;
@@ -52,6 +56,11 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mAccentPickerPref  = (Preference) screen.findPreference(KEY_ACCENT_PICKER_FRAGMENT_PREF);
+        if (!CandyUtils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+            mAccentPickerPref.setEnabled(true);
+        } else {
+            mAccentPickerPref.setEnabled(false);
+        }
     }
 
     @Override
@@ -79,16 +88,25 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
             new OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    AccentPicker.show(mParent);
-                    return true;
+                   if (!CandyUtils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+                        AccentPicker.show(mParent);
+                        return true;
+                   } else {
+                        return false;
+                   }
                 }
             });
     }
 
     public void updateSummary() {
         if (mAccentPickerPref != null) {
+            if (!CandyUtils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
                 mAccentPickerPref.setSummary(mContext.getString(
                         com.android.settings.R.string.theme_accent_picker_summary));
+            } else {
+                mAccentPickerPref.setSummary(mContext.getString(
+                        com.android.settings.R.string.disable_accents_installed_title));
+            }
         }
     }
 }

src/com/android/settings/display/SystemThemePreferenceController.java
@@ -18,26 +18,30 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.TextUtils;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.provider.Settings;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.android.settings.R;
-
 import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
+
+import com.android.internal.util.candy.CandyUtils;
 
 import libcore.util.Objects;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SystemThemePreferenceController extends AbstractPreferenceController implements
-        Preference.OnPreferenceChangeListener {
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
 
     private static final String SYSTEM_THEME = "system_theme_style";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private ListPreference mSystemThemeStyle;
 
     public SystemThemePreferenceController(Context context) {
@@ -58,14 +62,18 @@ public class SystemThemePreferenceController extends AbstractPreferenceControlle
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mSystemThemeStyle = (ListPreference) screen.findPreference(SYSTEM_THEME);
-        int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.SYSTEM_THEME, 0);
-        int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
-        mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
-        mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        if (!CandyUtils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+            int systemThemeStyle = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SYSTEM_THEME, 0);
+            int valueIndex = mSystemThemeStyle.findIndexOfValue(String.valueOf(systemThemeStyle));
+            mSystemThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mSystemThemeStyle.setSummary(mSystemThemeStyle.getEntry());
+            mSystemThemeStyle.setOnPreferenceChangeListener(this);
+        } else {
+            mSystemThemeStyle.setEnabled(false);
+            mSystemThemeStyle.setSummary(R.string.disable_themes_installed_title);
+        }
     }
-
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mSystemThemeStyle) {
@@ -80,7 +88,6 @@ public class SystemThemePreferenceController extends AbstractPreferenceControlle
         }
         return true;
     }
-
     private void reload(){
         Intent intent2 = new Intent(Intent.ACTION_MAIN);
         intent2.addCategory(Intent.CATEGORY_HOME);